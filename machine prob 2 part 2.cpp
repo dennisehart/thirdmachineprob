c7d582f914e2719c19348e3d6343b0897725661f
Dennise Coleen Co <55306196+dennisehart@users.noreply.github.com>
2020-08-27 18:13:30 +0800

    Add files via upload

---
+++ machine prob 2 part 2.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+int main ()
+
+{
+
+int ctr=0,num=50;
+
+    do   
+   {ctr=ctr+=5;
+   cout<<ctr<<" ";
+       } 
+     while (ctr<=num);
+
+
+return 0;
+	
+}
